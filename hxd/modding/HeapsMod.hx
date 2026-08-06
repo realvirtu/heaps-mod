8ca9f33d05ea32a65e71dce453b8d8dceed4ea15
realvirtu <103977942+realvirtu@users.noreply.github.com>
2026-08-06 16:39:22 -0400

    Basic asset replacement

--- hxd/modding/HeapsMod.hx
+++ hxd/modding/HeapsMod.hx
@@ -1,6 +1,28 @@
 package hxd.modding;
 
+typedef HeapsModConfig = {
+    ?modRoot:String,
+    ?mods:Array<String>
+}
+
 class HeapsMod
 {
-    public static function init() {}
+    public static var modRoot:String;
+
+    static var mods:Array<String> = [];
+    static var loader:Loader;
+
+    public static function init(config:HeapsModConfig)
+    {
+        modRoot = config.modRoot ?? 'mods';
+        mods = config.mods ?? [];
+
+        if (loader == null)
+            Res.loader = loader = new Loader();
+    }
+
+    public static function getActiveMods():Array<String>
+    {
+        return mods.copy();
+    }
 }
